5de32ff87bbeedb00f3b39d5ebed5b2c851b0783
faaat4735 <zjf580@163.com>
2022-09-09 17:12:42 +0800

    Delete getHtml.py

--- vendor/getHtml.py
+++
@@ -1,8 +0,0 @@
-from urllib import request
-
-def getHtml(url, headers):
-    if len(headers):
-        url = request.Request(url=url, headers=headers)
-    page = request.urlopen(url).read()
-    page = page.decode('GBK')
-    return page
